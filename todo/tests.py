55e9e9546b3e6da2537df52b00e2796dfa18216e
1F10250198 <s1f102501989@iniad.org>
2026-06-18 12:07:56 +0900

    ソート機能を追加

--- todo/tests.py
+++ todo/tests.py
@@ -52,20 +52,46 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
     
-    class TodoViewTestCase(TestCase) :
-        def test_index_get(self):
-            client = Client()
-            response = client. get('/')
+class TodoViewTestCase(TestCase) :
+    def test_index_get(self):
+        client = Client()
+        response = client. get('/')
             
-            self. assertEqual(response. status_code, 200)
-            self.assertEqual(response. templates[0]. name, 'todo/index.html')
-            self.assertEqual(len(response. context[ 'tasks' ]), 0)
+        self. assertEqual(response. status_code, 200)
+        self.assertEqual(response. templates[0]. name, 'todo/index.html')
+        self.assertEqual(len(response. context[ 'tasks' ]), 0)
         
-        def test_index_post(self):
-            client = Client()
-            data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
-            response = client. post('/', data)
+    def test_index_post(self):
+        client = Client()
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        response = client. post('/', data)
             
-            self.assertEqual(response. status_code, 200)
-            self. assertEqual(response. templates[0].name, 'todo/index.html')
-            self.assertEqual(len(response.context[ 'tasks' ]), 1)
+        self.assertEqual(response. status_code, 200)
+        self. assertEqual(response. templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context[ 'tasks' ]), 1)
+    
+    def test_index_get_order_post(self):
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1. save()
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2. save()
+        client = Client()
+        response = client. get('/?order=post')
+        
+        self.assertEqual(response. status_code, 200)
+        self. assertEqual(response. templates [0] .name, 'todo/index.html')
+        self. assertEqual(response. context[ 'tasks' ] [0], task2)
+        self. assertEqual(response. context[ 'tasks ' ] [1], task1)
+    
+    def test_index_get_order_due(self):
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1. save()
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2. save()
+        client = Client()
+        response = client. get('/?order=due')
+        
+        self.assertEqual(response. status_code, 200)
+        self. assertEqual(response. templates [0] . name, 'todo/index.html')
+        self. assertEqual(response. context[ 'tasks' ] [0], task1)
+        self. assertEqual(response.context[ 'tasks ' ] [1], task2)
