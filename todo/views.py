d6afe588c4dfacdf1daec96c0deea504d2d71f2e
1F10250198 <s1f102501989@iniad.org>
2026-06-25 11:30:00 +0900

    fix code

--- todo/views.py
+++ todo/views.py
@@ -1,20 +1,21 @@
-from django. shortcuts import render
-from django. utils. timezone import make_aware
-from django. utils. dateparse import parse_datetime
+from django.shortcuts import render
+from django.utils.dateparse import parse_datetime
+from django.utils.timezone import make_aware
 from todo.models import Task
 
+
 # Create your views here.
-def index(request) :
+def index(request):
     if request.method == 'POST':
-        task = Task(title=request. POST['title'],
-                    due_at=make_aware(parse_datetime(request. POST[ 'due_at' ])))
-        task. save()
-        
+        task = Task(title=request.POST['title'],
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task.save()
+
     if request.GET.get('order') == 'due':
-        tasks = Task. objects.order_by('due_at')
+        tasks = Task.objects.order_by('due_at')
     else:
-        tasks = Task. objects. order_by('-posted_at')
-    
+        tasks = Task.objects.order_by('-posted_at')
+
     context = {
         'tasks': tasks
     }
