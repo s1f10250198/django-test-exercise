7aa6fb50b58837ab1352559f0695a628add246fc
1F10250061 <iniad@sakuraPC.localdomain>
2026-07-09 11:21:47 +0900

    削除機能のテスト追加

--- todo/tests.py
+++ todo/tests.py
@@ -99,6 +99,22 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
 
+    def test_delete_get_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.get('/{}/delete'.format(task.pk))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/')
+        self.assertFalse(Task.objects.filter(pk=task.pk).exists())
+
+    def test_delete_get_fail(self):
+        client = Client()
+        response = client.get('/1/delete')
+
+        self.assertEqual(response.status_code, 404)
+
     def test_detail_get_success(self):
         task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task.save()
